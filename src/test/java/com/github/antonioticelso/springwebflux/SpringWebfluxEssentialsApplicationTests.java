50896c14cab927a66a8d7e22e62836c5a0b92019
Antonio <antonio.ti.celso@gmail.com>
2022-05-29 00:29:33 -0300

    First commit the project.

---
+++ src/test/java/com/github/antonioticelso/springwebflux/SpringWebfluxEssentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.antonioticelso.springwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxEssentialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
